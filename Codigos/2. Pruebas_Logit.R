df83ea18c1865334636f4418fb6c7013ea5ced13
HenryC2 <82994363+HenryC2@users.noreply.github.com>
2024-10-13 22:45:44 -0500

    Modificaciones al Logit

--- Codigos/2. Pruebas_Logit.R
+++ Codigos/2. Pruebas_Logit.R
@@ -5,10 +5,10 @@
 library(doParallel)
 num_cores <- parallel::detectCores()
 print(num_cores)
-cl <- makeCluster(num_cores - 3)  # Usar todos menos 3
+cl <- makeCluster(num_cores - 4)  # Usar todos menos 3
 registerDoParallel(cl)
 
-stopCluster(cl)    # Para liberar los nucleos 
+#stopCluster(cl)    # Para liberar los nucleos 
 
 
 
@@ -26,11 +26,11 @@ str(Train) # Confirmamos que la variable pobre es factor
 
 # Filtramos la base Train con las varaibles de interes
 train_filtro <- Train %>%
-  select(maxEducLevel, Dominio, n_cuartos, Nper, nocupados, Head_Ocupacion, Cabecera, Head_edad, Pobre)
+  select(maxEducLevel, Dominio, n_cuartos, Nper, nocupados, Cabecera,Head_Ocupacion, Head_edad, Pobre)
 
 # En Test, omitimos la variable Pobre
 test_filtro <- Test %>%
-  select(maxEducLevel, Dominio, n_cuartos, Nper, nocupados, Head_Ocupacion, Cabecera, Head_edad)
+  select(id,maxEducLevel, Dominio, n_cuartos, Nper, nocupados, Cabecera,Head_Ocupacion, Head_edad)
 
 
 
@@ -38,10 +38,9 @@ test_filtro <- Test %>%
 
 set.seed(12345)
 train_indices <- as.integer(createDataPartition(train_filtro$Pobre, p = 0.85, list = FALSE))
-train <- Train[train_indices, ]
-test <- Train[-train_indices, ]
+train <- train_filtro[train_indices, ]
+test <- train_filtro[-train_indices, ]
 prop.table(table(train$Pobre))
-prop.table(table(test$Pobre))
 
 
 # Modelo Logit ------------------------------------------------------------
@@ -66,20 +65,21 @@ modelo_logit$resample
 logit_accuracy<-modelo_logit$results$Accuracy
 logit_accuracy
 
-#Test
-test <- test  %>% 
-  mutate(Pobre =predict(modelo_logit,newdata = test,type = "raw")) %>%
-  select(id,Pobre)
+#Test interno
+test_1 <- test  %>% 
+  mutate(Pobre_hat =predict(modelo_logit,newdata = test,type = "raw"))
 
-confusionMatrix(data = test$hat_modelo_logit, 
-                reference = test$Pobre, positive="Yes", mode = "prec_recall")
+confusionMatrix(data = test_1$Pobre_hat, 
+                reference = test_1$Pobre, positive="Yes", mode = "prec_recall")
 
-# Se deja en el formato requerido
-Prediccion <- test %>% 
-  mutate(pobre=ifelse(Pobre=="Yes",1,0)) %>% 
+# Prediccion fuera de muestra
+Prediccion <- test_filtro  %>% 
+  mutate(Pobre =predict(modelo_logit,newdata = test_filtro,type = "raw"),
+         pobre=ifelse(Pobre=="Yes",1,0)) %>% 
   select(id,pobre)
 
-Nombre <- paste0("Logit_", ".csv") 
+
+Nombre <- paste0("Logit_Simple", ".csv") 
 setwd(paste0(wd,"\\Output\\Logit"))
 write.csv(Prediccion,Nombre, row.names = FALSE)
 
@@ -95,7 +95,7 @@ fiveStats <- function(...)  c(defaultSummary(...),  prSummary(...))  ## Para
 
 
 ctrl<- trainControl(method = "cv",
-                    number = 5,
+                    number = 10,
                     classProbs = TRUE,
                     summaryFunction = fiveStats,
                     savePredictions = T)
@@ -116,30 +116,41 @@ modelo_logit_F$resample
 logit_F<-modelo_logit_F$results$F
 logit_F
 
-#Test
-test <- test  %>% mutate(hat_modelo_logit_F=predict(modelo_logit_F,newdata = test,
-                                                      type = "raw"))
-confusionMatrix(data = test$hat_modelo_logit_F, 
-                reference = test$Pobre, positive="Yes", mode = "prec_recall")
+
+#Test interno
+test_2 <- test  %>% 
+  mutate(Pobre_hat =predict(modelo_logit_F,newdata = test,type = "raw"))
+
+confusionMatrix(data = test_2$Pobre_hat, 
+                reference = test_2$Pobre, positive="Yes", mode = "prec_recall")
+
+# Prediccion fuera de muestra
+Prediccion_2 <- test_filtro  %>% 
+  mutate(Pobre =predict(modelo_logit_F,newdata = test_filtro,type = "raw"),
+         pobre=ifelse(Pobre=="Yes",1,0)) %>% 
+  select(id,pobre)
 
 
-
+Nombre_2 <- paste0("Logit_F", ".csv") 
+setwd(paste0(wd,"\\Output\\Logit"))
+write.csv(Prediccion_2,Nombre_2, row.names = FALSE)
 
 
 
 # Modelo rebalanceado ------------------------------------------------------------
 # Re balanceo con SMOTE  --------------------------------------------------
 
-predictors<-colnames(train_filtro  %>% select(-Pobre))
+# 2.4.1 Re balanceo usando el Train original 
+Predictores <- train %>%
+  select(-Pobre) %>%
+  mutate_if(is.factor, as.numeric) %>%  # Convierte factores a numérico
+  mutate_if(is.character, as.numeric) %>% # Convierte caracteres a numérico
+  as.data.frame()
 
-#Convertir a dummies
-Predictores_num <- Predictores %>%
-  mutate_if(is.factor, as.numeric) 
+SMOTE <- SMOTE(X = Predictores,target = train$Pobre,K=5)
+Train_SMOTE <- SMOTE$data
 
-# Ajuste SMOTE
-smote_output <- SMOTE(X = Predictores_num,
-                      target = train_filtro$Pobre, K = 5)
-Train_SMOTE <- smote_output$data
+setwd(paste0(wd,"\\Base\\Base_Elastic_Net"))
 
 
 
@@ -157,12 +168,34 @@ logit_smote$resample
 logit_SMOTE_F<-logit_smote$results$F
 logit_SMOTE_F
 
-#Test
-test <- test  %>% mutate(hat_modelo_logit_smote_F=predict(logit_SMOTE_F,newdata = test,
-                                                    type = "raw"))
-confusionMatrix(data = test$hat_modelo_logit_smote_F, 
-                reference = test$Pobre, positive="Yes", mode = "prec_recall")
+#Test interno
+pobre_col <- test %>% select(Pobre)
+otras_columnas <- test %>% select(-Pobre)
+otras_columnas_transformadas <- otras_columnas %>%
+  mutate_if(is.factor, as.numeric) %>%  # Convierte factores a numérico
+  mutate_if(is.character, as.numeric)   # Convierte caracteres a numérico
+test_SMOTE <- bind_cols(pobre_col, otras_columnas_transformadas) # Volver a unir 
+test_SMOTE <- as.data.frame(test_SMOTE)
+
+test_3 <- test_SMOTE  %>% 
+  mutate(Pobre_hat =predict(logit_smote,newdata = test_SMOTE,type = "raw"))
+
+confusionMatrix(data = test_3$Pobre_hat, 
+                reference = test_3$Pobre, positive="Yes", mode = "prec_recall")
+
+# Prediccion fuera de muestra
+test_filtro_SMOTE <- test_filtro  %>% 
+  mutate_if(is.factor, as.numeric) %>%  # Convierte factores a numérico
+  mutate_if(is.character, as.numeric) %>% # Convierte caracteres a numérico
+  as.data.frame()
+  
+Prediccion_3 <- test_filtro_SMOTE %>% 
+  mutate(Pobre =predict(logit_smote,newdata = test_filtro_SMOTE,type = "raw"),
+         pobre=ifelse(Pobre=="Yes",1,0)) %>% 
+  select(id,pobre)
 
 
-
+Nombre_3 <- paste0("LogitSmote_F_", ".csv") 
+setwd(paste0(wd,"\\Output\\Logit"))
+write.csv(Prediccion_3,Nombre_3, row.names = FALSE)
 
